8a05c2af1cbcfa1eac9532f2d7a0f4acfd9bf17e
Isaque Böck <isaquebockdos@gmail.com>
2024-06-28 23:54:41 -0300

    build: change environment api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    API: 'https://backend.boostingdevs.com',
+    API: 'https://seal-app-rpul5.ondigitalocean.app',
     supabaseUrl: 'https://ujmvnzcbcgavpknmqcfx.supabase.co',
     supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqbXZuemNiY2dhdnBrbm1xY2Z4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU3MjkyNTEsImV4cCI6MjAzMTMwNTI1MX0.B0AxNh-3Z2ZrZc5F2xnHetG2deK1MXWaHbtbsRr9vPo'  
   };
